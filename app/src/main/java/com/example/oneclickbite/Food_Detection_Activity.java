296fe48559dbcff3dc719e8f35a159f719f9264a
Devansh <devansh22097@iiitd.ac.in>
2023-03-03 15:11:29 +0530

    added 2 buttons and going back to previous act

--- app/src/main/java/com/example/oneclickbite/Food_Detection_Activity.java
+++ app/src/main/java/com/example/oneclickbite/Food_Detection_Activity.java
@@ -6,15 +6,19 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Food_Detection_Activity extends AppCompatActivity {
 
     ImageView FoodImg;
     TextView txtLabel;
     RadioButton radioGet, radioOrder;
+    Button cook1,order1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,7 +28,8 @@ public class Food_Detection_Activity extends AppCompatActivity {
         txtLabel = findViewById(R.id.txtLabel);
         radioGet = findViewById(R.id.radioYes);
         radioOrder = findViewById(R.id.radioNo);
-
+        cook1=findViewById(R.id.cook);
+        order1=findViewById(R.id.order);
 
         Intent iGet = getIntent();
         if(iGet.hasExtra("image")){
@@ -32,6 +37,21 @@ public class Food_Detection_Activity extends AppCompatActivity {
            Bitmap imgGet = BitmapFactory.decodeByteArray(bImageArrayGet, 0, bImageArrayGet.length);
            FoodImg.setImageBitmap(imgGet);
         }
-
+radioGet.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View view) {
+        cook1.setVisibility(View.VISIBLE);
+        order1.setVisibility(View.VISIBLE);
+        radioOrder.setChecked(false);
+    }
+});
+        radioOrder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(Food_Detection_Activity.this, "Please click the image again", Toast.LENGTH_SHORT).show();
+                Intent temp = new Intent(Food_Detection_Activity.this, MainActivity.class);
+                startActivity(temp);
+            }
+        });
     }
 }
