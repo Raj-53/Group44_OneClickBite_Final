0c052056349fc2703e1def1c46379e680e0200ca
Raj <113069572+Raj-53@users.noreply.github.com>
2023-04-12 12:40:02 +0530

    merge Gunjan update

--- app/src/main/java/com/example/oneclickbite/Food_Detection_Activity.java
+++ app/src/main/java/com/example/oneclickbite/Food_Detection_Activity.java
@@ -7,7 +7,6 @@ import androidx.fragment.app.FragmentTransaction;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.content.res.Configuration;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
